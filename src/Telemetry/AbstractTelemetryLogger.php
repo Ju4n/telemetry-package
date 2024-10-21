36847094238f3247d05d2865779a8fbfe9bda2d9
Juan Deladoey <juan@billomat.com>
2024-10-22 01:09:38 +0200

    add php fixed settings and AbstractTelemetryLogger

---
+++ src/Telemetry/AbstractTelemetryLogger.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Telemetry;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use InvalidArgumentException;
+use Psr\Log\AbstractLogger;
+use Stringable;
+
+abstract class AbstractTelemetryLogger extends AbstractLogger
+{
+    public function __construct(protected DateTimeZone $dateTimezone)
+    {
+    }
+
+    protected function validateLevel($level): Level
+    {
+        $level = Level::tryFrom($level);
+        if (!$level) {
+            throw new InvalidArgumentException(
+                \sprintf("Level should be an PSR-3 level compatible (%s)", implode(', ', Level::getLevels()))
+            );
+        }
+
+        return $level;
+    }
+
+    protected function createLogEntry($level, string|Stringable $message, array $context = []): LogEntry
+    {
+        $level = $this->validateLevel($level);
+        $dateTime = new DateTimeImmutable('now', $this->dateTimezone);
+
+        return new LogEntry($dateTime, $level, $message, $context);
+    }
+}
